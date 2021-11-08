f3875a99cd6e88dd5c6aab00243b74f9210828fd
AdmiralRaza <Matteo20044$>
2021-11-08 10:41:54 +0100

    hei

---
+++ app/src/main/java/com/example/horoscope/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.horoscope
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.lang.IndexOutOfBoundsException
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val spinnerSign: Spinner = findViewById(R.id.spinner_sign)
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.signs,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerSign.adapter = adapter
+        }
+
+
+        val spinnerDay: Spinner = findViewById(R.id.spinner_day)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.days,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerDay.adapter = adapter
+        }
+
+        val start : Button = findViewById(R.id.start)
+
+        start.setOnClickListener() {
+            val setSign = spinnerSign.selectedItem.toString()
+            val setDay = spinnerDay.selectedItem.toString()
+
+            val intent = Intent(Intent(this@MainActivity, HoroskopActivity::class.java))
+            intent.putExtra("url", createURL(setSign, setDay))
+            startActivity(intent)
+        }
+
+
+
+
+    }
+
+    private fun createURL(sign : String, date: String) : String {
+        return "https://aztro.sameerkumar.website/?sign=$sign&day=$date"
+    }
+}
